ab44ec4c6ce8e1d6e3ef70c1cbcd9f9a2b36df7d
Wes <114735316+Wes34840@users.noreply.github.com>
2023-11-21 12:04:40 +0100

    Added Comment

--- M6BO-Project/Assets/PlayerMovement.cs
+++ M6BO-Project/Assets/PlayerMovement.cs
@@ -16,6 +16,7 @@ public class PlayerMovement : MonoBehaviour
     }
 
     public void OnMove(InputAction.CallbackContext ctx)
+    // ctx = the value that is given when the action is called, you can see the type of the variable in the Player Input Actions, the Action you are calling and looking at the "Control Type"
     {
         Vector3 horizontalInput = ctx.ReadValue<Vector3>();
         rb.velocity = horizontalInput * playerStats.movementSpeed;
