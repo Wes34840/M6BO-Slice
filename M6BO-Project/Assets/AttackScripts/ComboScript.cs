713eb0f1591a2fdcbfed472125dccd82c3e58d0c
Safwan-Almashnouk <114736059+Safwan-Almashnouk@users.noreply.github.com>
2023-11-22 12:11:41 +0100

    Bugs fixed

--- M6BO-Project/Assets/AttackScripts/ComboScript.cs
+++ M6BO-Project/Assets/AttackScripts/ComboScript.cs
@@ -7,6 +7,8 @@ public class ComboScript : MonoBehaviour
     private Animator animator;
     private bool shouldGoNextCombo = false;
     private bool isAttacking;
+    private bool HeavyCombo = false;
+   
 
     void Start()
     {
@@ -18,19 +20,30 @@ public class ComboScript : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.E)) 
         { 
             shouldGoNextCombo = true;
-            
+        }
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            HeavyCombo = true;
 
         }
-
         ShouldGoNextCombo(shouldGoNextCombo);
-        
+        HeavyCombos(HeavyCombo);
         Debug.Log(isAttacking);
+        
+    }
+ 
+
+    private void HeavyCombos(bool value)
+    {
+        animator.SetBool("HeavyCombo", value);
     }
 
     public void AnimationStarted()
     {
         isAttacking= true;
         shouldGoNextCombo = false;
+        HeavyCombo= false;
+        
     }
 
     private void ShouldGoNextCombo(bool value)
