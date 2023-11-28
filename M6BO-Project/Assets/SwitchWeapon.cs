113abe916712f05cbfeb7e97893a25334ba7aab5
Safwan-Almashnouk <114736059+Safwan-Almashnouk@users.noreply.github.com>
2023-11-28 10:46:48 +0100

    animator

---
+++ M6BO-Project/Assets/SwitchWeapon.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchWeapon : MonoBehaviour
+{
+    public List<RuntimeAnimatorController> controllers= new List<RuntimeAnimatorController>();
+    public GameObject[] weapons;
+
+    private void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            weapons
+        }
+    }
+}
