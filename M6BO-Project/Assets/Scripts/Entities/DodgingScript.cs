503b395246118d2d669e752ad90b4ec767fdd35d
SuFo <114589809+ElizaVoets@users.noreply.github.com>
2023-12-13 12:05:25 +0100

    Start of the dodging

---
+++ M6BO-Project/Assets/Scripts/Entities/DodgingScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class DodgingScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+
+    void Start()
+    {
+
+    }
+
+    public void DodgingFunction()
+    {
+
+    }
+
+
+}
