57c414bb056a2f251931cc23491266e6e050e886
SuFo <114589809+ElizaVoets@users.noreply.github.com>
2023-12-14 10:10:21 +0100

    Bugs.

--- M6BO-Project/Assets/Scripts/Entities/DodgingScript.cs
+++ M6BO-Project/Assets/Scripts/Entities/DodgingScript.cs
@@ -1,18 +1,37 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class DodgingScript : MonoBehaviour
 {
     // Start is called before the first frame update
 
 
-    void Start()
-    {
+    public Animator animator;
+    BoxCollider HitBox;
 
+    public void Start()
+    {
+        HitBox = transform.GetChild(2).GetComponent<BoxCollider>();
+        animator = GetComponent<Animator>();
     }
 
-    public void DodgingFunction()
-    {
 
+    public void DodgeChildSupport(InputAction.CallbackContext ctx)
+    {
+        if (ctx.ReadValue<float>() == 0) return;
+        Debug.Log("called");
+        animator.SetBool("isDodging", true);
+        float animDuration = animator.runtimeAnimatorController.animationClips.First(i => i.name == "Dodge").length;
+        StartCoroutine(WaitForAnimLength(animDuration));
+        // Trigger I-Frames here
+
+    }
+    public IEnumerator WaitForAnimLength(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        animator.SetBool("IsDodging", false);
     }
 
 
