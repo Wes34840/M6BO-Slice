91672a9014fe500b938d703920f3b71f2fd9a325
Wes <114735316+Wes34840@users.noreply.github.com>
2024-01-17 10:09:27 +0100

    Squashed commit of the following:
    commit 45745cf2977467b64916efdcff2de6b2d8a72556
    Author: Wes <114735316+Wes34840@users.noreply.github.com>
    Date:   Wed Jan 17 10:05:28 2024 +0100
    
        Bug fixes caused by code conventions
    
        - All variables defined
        - ComboScript has been made SRP-compliant
        - Made names more readable
    
    commit b7ab392cfb274bc183060e0508ed6a5334c8778c
    Author: Wes <114735316+Wes34840@users.noreply.github.com>
    Date:   Tue Jan 16 14:31:32 2024 +0100
    
        Code changes
    
        - Will fix any variables in the inspector tomorrow

--- M6BO-Project/Assets/Scripts/Enemy/RangeEnemyPath.cs
+++ M6BO-Project/Assets/Scripts/Enemy/RangeEnemyPath.cs
@@ -1,20 +1,21 @@
-using System.Collections;
+﻿using System.Collections;
 using UnityEngine;
 
 public class RangeEnemyPath : PathingAI
 {
-    public int maxDist;
-    public bool isFleeing = false;
-    public GameObject playNode, runNode;
-    public EnemyRangeAttack attack;
+    [SerializeField] private int _maxDist;
+    private bool _isFleeing = false;
+    private EnemyRangeAttack _attackScript;
+
     public override void Start()
     {
         GetAll();
-        attack = GetComponent<EnemyRangeAttack>();
+        _attackScript = GetComponent<EnemyRangeAttack>();
     }
+
     public override void CheckForPlayer()
     {
-        Transform target = fov.FindVisibleTargets();
+        Transform target = enemyfov.FindVisibleTargets();
         if (target != null && agent.enabled)
         {
             Vector3 targetPos = target.position;
@@ -23,22 +24,22 @@ public class RangeEnemyPath : PathingAI
                 agent.isStopped = false;
                 awoken = true;
             }
-            fov.viewAngle = 360;
+            enemyfov.viewAngle = 360;
             aggroTimer = 20;
 
-            if (isFleeing) return;
+            if (_isFleeing) return;
 
-            agent.stoppingDistance = maxDist + 5;
+            agent.stoppingDistance = _maxDist + 5;
 
             agent.SetDestination(targetPos);
-            playNode.transform.position = targetPos;
             CheckIfShouldFlee();
-            if (isFleeing) return;
+            if (_isFleeing) return;
             CheckIfShouldShoot(target);
 
         }
         else agent.stoppingDistance = 0;
     }
+
     public override void HandleRotation()
     {
         if (agent.remainingDistance < agent.stoppingDistance) model.rotation = UpdateDirection(agent.destination);
@@ -47,25 +48,26 @@ public class RangeEnemyPath : PathingAI
 
     private void CheckIfShouldFlee()
     {
-        if (agent.remainingDistance < maxDist && (agent.destination - transform.position).magnitude < maxDist)
+        if (agent.remainingDistance < _maxDist && (agent.destination - transform.position).magnitude < _maxDist)
         {
-            isFleeing = true;
+            _isFleeing = true;
             Vector3 dest = (agent.destination - transform.position);
             agent.SetDestination(-dest * 5); // run away from player in opposite direction
-            runNode.transform.position = agent.destination;
             StartCoroutine(FleeFromPlayer());
         }
     }
+
     private void CheckIfShouldShoot(Transform target)
     {
-        if (!attack.onCooldown) attack.ShootProjectile(target);
+        if (!_attackScript.onCooldown) _attackScript.ShootProjectile(target);
     }
+
     private IEnumerator FleeFromPlayer()
     {
         agent.stoppingDistance = 0;
         yield return new WaitForSeconds(2);
-        isFleeing = false;
-        agent.stoppingDistance = maxDist + 5;
+        _isFleeing = false;
+        agent.stoppingDistance = _maxDist + 5;
     }
 
 }
--- M6BO-Project/Assets/Scripts/Entities/Combat/EntityHitbox.cs
+++ M6BO-Project/Assets/Scripts/Entities/Combat/EntityHitbox.cs
@@ -2,15 +2,15 @@
 
 public class EntityHitbox : MonoBehaviour
 {
-    public EntityStats stats;
-    public EntityPoise poise;
+    private EntityStats _entityStats;
+    private EntityPoise _poiseScript;
     public bool isBlocking;
     public bool isDodging;
 
     public void TakeDamage(WeaponStats weapon)
     {
         PlayAudio();
-        /*
+
         transform.parent.TryGetComponent(out PathingAI path);
         if (path != null) path.TryWakeUp();
         if (isDodging) return;
@@ -19,31 +19,32 @@ public class EntityHitbox : MonoBehaviour
         Debug.Log("Triggered Damage");
         if (isBlocking)
         {
-            stats.health -= damage * (stats.blockingPower / 10);
-            poise.CurrentPoise -= poiseDamage * (stats.blockingPower / 10);
+            _entityStats.health -= damage * (_entityStats.blockingPower / 10);
+            _poiseScript.currentPoise -= poiseDamage * (_entityStats.blockingPower / 10);
         }
+
         else
         {
-            stats.health -= damage;
-            poise.CurrentPoise -= poiseDamage;
+            _entityStats.health -= damage;
+            _poiseScript.currentPoise -= poiseDamage;
             Debug.Log(damage);
             Debug.Log(poiseDamage);
         }
-        */
+
     }
 
     public void PlayAudio()
     {
-        
+
     }
+
     public float CalculateDamage(WeaponStats weapon)
     {
         Debug.Log(weapon.damage);
         Debug.Log(weapon.currentState);
         return weapon.damage * (int)weapon.currentState;
-        
     }
-    // this feels extremely unncsessary 
+    // this feels extremely unnecessary 
     public float CalculatePoise(WeaponStats weapon)
     {
         return weapon.poiseDamage * (int)weapon.currentState;
